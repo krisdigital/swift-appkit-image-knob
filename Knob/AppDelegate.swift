3f19714f2efff49e38cd8997c387cd1d8b05d613
krisdigital <info@krisdigital.com>
2017-11-10 19:41:35 +0100

    Initial Commit

---
+++ Knob/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Knob
+//
+//  Created by Christopher Frank on 10.11.17.
+//  Copyright © 2017 krisdigital. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Knob/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Knob
+//
+//  Created by Christopher Frank on 10.11.17.
+//  Copyright © 2017 krisdigital. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
